192c16d3007677e95c92424c6e882fa66c368c31
SiriusRalph <sarahaylin787@gmail.com>
2026-04-02 01:35:48 +0100

    remove email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable 
 {
     use HasApiTokens, HasFactory, Notifiable;
 
